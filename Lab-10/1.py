ac61069e4d54fdf808e000e59b2024600b91b652
Sujan00001 <sujanrana630@gmail.com>
2025-04-30 22:28:48 +0530

    Update 1.py

--- Lab-10/1.py
+++ Lab-10/1.py
@@ -3,9 +3,9 @@ with open("demo.csv",'w+',newline='')as f1:
     wri=csv.writer(f1)
 
     str1=[['Roll No','Name','Mark1','Mark2','Mark3','Total'],
-      ['24BCP198','Krishnu',12,23,45,80],
-      ['24BCP199','Jack',12,23,45,80],
-      ['24BCP197','Roman',12,23,45,80]]
+      ['24BCP193','Rana',20,22,43,84],
+      ['24BCP191','Jack',21,20,44,82],
+      ['24BCP195','Rusi',22,23,45,80]]
 
     wri.writerows(str1) 
 f1.close()
